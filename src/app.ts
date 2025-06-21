93640100c34863b5f0ea4141b68759e4d06c4ff0
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-06-21 18:49:42 +0500

    working on pipelines

--- src/app.ts
+++ src/app.ts
@@ -81,13 +81,7 @@ app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["*"], // TODO: Replace '*' with your frontend URL
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  })
-);
+app.use(cors());
 
 // Routes
 
