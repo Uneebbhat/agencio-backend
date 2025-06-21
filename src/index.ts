8aee2a7812a1c9b0bac8d304ad366fb777d90d84
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-06-21 20:59:13 +0500

    changes made

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import { PORT } from "./config/constants";
-import logger from "./utils/logger";
-
-const port = PORT;
-
-app.listen(port, () => {
-  logger.info(`🚀 Server is running on http://localhost:${port} 🌍`);
-});
